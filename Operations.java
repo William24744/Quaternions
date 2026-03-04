7c68146e21da395ead68040935447ef99896456f
William24744 <99320@student.baisd.net>
2026-03-04 14:13:20 -0500

    .
    .

---
+++ Operations.java
@@ -0,0 +1,11 @@
+public class Operations {
+    int a;
+    int b;
+    int c;
+    int d;
+    public void ChooseEquation() throws IllegalArgumentException {
+        System.out.println("Choose a value");
+        System.out.println("Choose b value");
+        
+    }
+}
---
+++ Quaternions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Quaternions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean cleared = true;
+        Operations opp = new Operations();
+        while (true) {
+            if (cleared == true) {
+                try {
+                    
+                }
+            }
+            System.out.println("1.Add/Subtract, 2.Multiply, 3.Divide, 4.Exponent, 5.Natural Logarithm, 6.Absolute Value, 7.Clear, 8.Exit Program");
+        }
+    }
+}
